8cd06c036702be006c321ce8e2cad03170e353ab
ZengJunFeng <zengjunfeng9981@163.com>
2020-03-31 11:44:46 +0800

    upd

--- src/main/java/com/nero/mybatis/cache/Test.java
+++ src/main/java/com/nero/mybatis/cache/Test.java
@@ -1,8 +1,7 @@
 package com.nero.mybatis.cache;
 
-import com.nero.mybatis.cache.po.UserPO;
 import com.nero.mybatis.cache.mapper.UserMapper;
-import com.nero.mybatis.cache.pojo.UserParam;
+import com.nero.mybatis.cache.po.UserPO;
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
@@ -10,9 +9,7 @@ import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 public class Test {
     public static void main(String[] args) throws IOException {
